c103f3328c331845055b2e5ab9884eab115198e8
ramya <ramya.m@spritle.com>
2019-11-18 22:31:49 +0530

    Mail send funcion

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({origin: true});
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+/**
+* Here we're using Gmail to send 
+*/
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'ramyamahendran96@gmail.com',
+        pass: 'mahendran96'
+    }
+});
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      
+        // getting dest email by query string
+        const dest = req.query.dest;
+
+        const mailOptions = {
+            from: 'ramya mahendran <ramyamahendran96@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
+            to: dest,
+            subject: 'Welcome', // email subject
+            html: `<p style="font-size: 16px;">Hi,</p>
+                <br />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcX2Z4IR_RJZpSOfW5UkyE-mXXYVV3DJc5otwHZFvkD9BkniLS&s" />
+            ` // email content in HTML
+        };
+  
+        // returning result
+        return transporter.sendMail(mailOptions, (erro, info) => {
+            if(erro){
+                return res.send(erro.toString());
+            }
+            return res.send('Sended');
+        });
+    });    
+});
